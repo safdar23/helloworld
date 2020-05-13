2e433bb77f38ce81b69a53c3e73cf01444093d85
safdar23 <safdar23@yahoo.com>
2020-05-13 05:27:26 +0000

    modified from linux

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helo jhjhjhi changed by me");
+		System.out.println("helo jhjhjhi changed by safdar the great:w");
 		
 
 	}
